c8cd3bd503c761cce9d1c62c79777323c99bd541
Igor Lino <i.lino@controlexpert.com>
2015-05-29 13:30:52 +0200

    Initial implementation.

---
+++ js/widget-ezplus-directive.js
@@ -0,0 +1,54 @@
+(function () {
+    'use strict';
+
+    angular.module('ezplus')
+        .directive('ezPlus', ezPlus);
+
+    function ezPlus() {
+        var service = {
+            restrict: 'A',
+            scope: {
+                ngModel: '='
+            },
+            link: link
+        };
+        return service;
+
+        ////////////////////////////
+
+        
+		link.$inject = ['scope', 'element'];
+        function link(scope, element) {
+            scope.$watch('ngModel', function (image) {
+                var thumbMediumUrl = (image && image.thumbMediumUrl) || '';
+                var fullSizeUrl = (image && image.fullSizeUrl) || '';
+
+                var plugin = angular.element(element).data('ezPlus');
+                if (plugin) {
+                    var loader = 'images/loader-small.gif';
+                    plugin.swaptheimage(loader, loader);
+                    plugin.swaptheimage(thumbMediumUrl, fullSizeUrl);
+                } else if (image) {
+                    element.attr('src', thumbMediumUrl);
+                    element.attr('data-zoom-image', fullSizeUrl);
+                    angular.element(element).elevateZoom({
+                        scrollZoom: true,
+                        zoomWindowWidth: 600,
+                        zoomWindowHeight: 600,
+                        easing : true,
+                        zoomWindowFadeIn: 500,
+                        zoomWindowFadeOut: 500,
+                        lensFadeIn: 500,
+                        lensFadeOut: 500
+                    });
+                }
+            });
+
+            scope.$on('$destroy', function () {
+                element.remove();
+            });
+        }
+    }
+
+})
+();

644b1afce91f8260806578a18bebc473ffa62eab
Igor Lino <i.lino@directica.pl>
2015-06-25 00:52:53 +0200

    Ported to angular main page example.

--- demo/js/app.js
+++ demo/js/app.js
@@ -6,8 +6,8 @@ angular.module('elevatezoomdemo', [
 ])
 
     .run(
-    [          '$rootScope', '$state', '$stateParams',
-        function ($rootScope,   $state,   $stateParams) {
+    ['$rootScope', '$state', '$stateParams',
+        function ($rootScope, $state, $stateParams) {
 
             // It's very handy to add references to $state and $stateParams to the $rootScope
             // so that you can access them from any scope within your applications.
@@ -17,7 +17,7 @@ angular.module('elevatezoomdemo', [
     ]
 )
 
-    .config(function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
         //
         // For any unmatched url, redirect to /state1
         $urlRouterProvider.otherwise("/main");
@@ -52,5 +52,69 @@ angular.module('elevatezoomdemo', [
 
         function DemoCtrl($timeout, $scope, fancyboxService) {
             CueCtrl($timeout);
+
+            $scope.zoomOptionsGallery01 = {
+                scrollZoom: true,
+                zoomWindowWidth: 600,
+                zoomWindowHeight: 600,
+                easing: true,
+                zoomWindowFadeIn: 500,
+                zoomWindowFadeOut: 500,
+                lensFadeIn: 500,
+                lensFadeOut: 500,
+
+                initial: 'small',
+
+                gallery: 'gallery_01',
+                cursor: 'pointer',
+                galleryActiveClass: "active",
+                imageCrossfade: true,
+                loadingIcon: false
+            };
+
+            $scope.imagesForGallery = [];
+            $scope.setApproot = function(appRoot) {
+                //only change when needed.
+                if ($scope.approot && appRoot === $scope.approot) {
+                    return;
+                }
+                $scope.approot = appRoot;
+                $scope.imagesForGallery = [
+                    {
+                        thumb: appRoot + 'images/thumb/image1.jpg',
+                        small: appRoot + 'images/small/image1.jpg',
+                        large: appRoot + 'images/large/image1.jpg'
+                    },
+                    {
+                        thumb: appRoot + 'images/thumb/image2.jpg',
+                        small: appRoot + 'images/small/image2.jpg',
+                        large: appRoot + 'images/large/image2.jpg'
+                    },
+                    {
+                        thumb: appRoot + 'images/thumb/image3.jpg',
+                        small: appRoot + 'images/small/image3.jpg',
+                        large: appRoot + 'images/large/image3.jpg'
+                    },
+                    {
+                        thumb: appRoot + 'images/thumb/image4.jpg',
+                        small: appRoot + 'images/small/image4.jpg',
+                        large: appRoot + 'images/large/image4.jpg'
+                    },
+                    {
+                        thumb: appRoot + 'images/thumb/image5.jpg',
+                        small: appRoot + 'images/small/image5.jpg',
+                        large: appRoot + 'images/large/image5.jpg'
+                    }
+                ];
+
+                $scope.zoomModel1 = $scope.imagesForGallery[0];
+                $scope.zoomModel2 = $scope.imagesForGallery[1];
+
+                $scope.zoomModelGallery01 = $scope.imagesForGallery[1];
+                $scope.zoomModelGallery04 = $scope.imagesForGallery[1];
+            };
+
+            //default
+            $scope.setApproot('');
         }
     });
